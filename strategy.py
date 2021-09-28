2fc2e1be6236fb0b8971024edfe16d526c2bbff4
adamvizly <asghari.ali10@gmail.com>
2021-09-28 22:56:54 +0330

    add back test for strategies and improve code

--- strategy.py
+++ strategy.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-from ta.utils import dropna
-from ta.trend import SMAIndicator, MACD
+from ta.trend import MACD
 from ta.momentum import RSIIndicator
 from ta.volatility import AverageTrueRange, BollingerBands
     
@@ -14,8 +13,11 @@ class Strategy:
     
     def apply_indicator(self) -> None:
         pass
-
+    
     def action(self) -> str:
+        pass
+
+    def back_test(self, candles_before) -> list:
         pass
 
 
@@ -26,14 +28,26 @@ class RSI(Strategy):
     
     def apply_indicator(self) -> None:
         self.df['RSI'] = self.rsi.rsi()
-        self.latest_candle = self.df.iloc[[-1]]
-        self.second_latest_candle = self.df.iloc[[-2]]
+        self.latest_candle = self.df.iloc[-1]
+        self.second_latest_candle = self.df.iloc[-2]
     
     def action(self, upperbound, lowerbound) -> str:
-        if self.latest_candle['RSI'].values[0] > upperbound:
+        if self.latest_candle['RSI'] > upperbound:
             return 'sell'
-        elif self.latest_candle['RSI'].values[0] < lowerbound:
+        elif self.latest_candle['RSI'] < lowerbound:
             return 'buy'
+    
+    def back_test(self, candles_before, upperbound, lowerbound) -> list:
+        test_df = self.df.iloc[-candles_before:]
+        actions = []
+        for index, row in test_df.iterrows():
+            if row['RSI'] > upperbound:
+                actions.append((datetime.fromtimestamp(row['time']).strftime('%Y-%m-%d %H:%M:%S'), row['RSI'], 'sell'))
+            elif row['RSI'] < lowerbound:
+                actions.append((datetime.fromtimestamp(row['time']).strftime('%Y-%m-%d %H:%M:%S'), row['RSI'], 'buy'))
+            else:
+                actions.append((datetime.fromtimestamp(row['time']).strftime('%Y-%m-%d %H:%M:%S'), row['RSI'], 'hold'))
+        return actions
 
 
 class MACDIndex(Strategy):
@@ -44,8 +58,8 @@ class MACDIndex(Strategy):
     def apply_indicator(self) -> None:
         self.df['MACD'] = self.macd.macd()
         self.df['MACDS'] = self.macd.macd_signal()
-        self.latest_candle = self.df.iloc[[-1]]
-        self.second_latest_candle = self.df.iloc[[-2]]
+        self.latest_candle = self.df.iloc[-1]
+        self.second_latest_candle = self.df.iloc[-2]
     
     def action(self) -> str:
         if self.latest_candle['MACD'] > self.latest_candle['MACDS']:
@@ -53,6 +67,17 @@ class MACDIndex(Strategy):
         elif self.latest_candle['MACD'] < self.latest_candle['MACDS']:
             return 'sell'
 
+    def back_test(self, candles_before) -> list:
+        test_df = self.df.iloc[-candles_before:]
+        actions = []
+        for index, row in test_df.iterrows():
+            if row['MACD'] < row['MACDS']:
+                actions.append((datetime.fromtimestamp(row['time']).strftime('%Y-%m-%d %H:%M:%S'), row['MACD'], row['MACDS'], 'sell'))
+            elif row['MACD'] > row['MACDS']:
+                actions.append((datetime.fromtimestamp(row['time']).strftime('%Y-%m-%d %H:%M:%S'), row['MACD'], row['MACDS'], 'buy'))
+            else:
+                actions.append((datetime.fromtimestamp(row['time']).strftime('%Y-%m-%d %H:%M:%S'), row['MACD'], row['MACDS'], 'hold'))
+        return actions
 
 class ATR(Strategy):
     def __init__(self, df, period) -> None:
@@ -61,11 +86,11 @@ class ATR(Strategy):
     
     def apply_indicator(self) -> None:
         self.df['ATR'] = self.atr.average_true_range()
-        self.latest_candle = self.df.iloc[[-1]]
-        self.second_latest_candle = self.df.iloc[[-2]]
+        self.latest_candle = self.df.iloc[-1]
+        self.second_latest_candle = self.df.iloc[-2]
 
     def get_atr(self, count) -> float:
-        return self.latest_candle['ATR'].values[0] * count
+        return self.latest_candle['ATR'] * count
 
 
 class BollingerBand(Strategy):
@@ -78,15 +103,56 @@ class BollingerBand(Strategy):
         self.df['BBL'] = self.bb.bollinger_lband()
         self.df['BBHI'] = self.bb.bollinger_hband_indicator()
         self.df['BBLI'] = self.bb.bollinger_lband_indicator()
-        self.latest_candle = self.df.iloc[[-1]]
-        self.second_latest_candle = self.df.iloc[[-2]]
+        self.latest_candle = self.df.iloc[-1]
+        self.second_latest_candle = self.df.iloc[-2]
 
     def action(self) -> str:
-        if self.latest_candle['BBHI'].values[0]:
+        if self.latest_candle['BBHI']:
             return 'sell'
-        elif self.latest_candle['BBLI'].values[0]:
+        elif self.latest_candle['BBLI']:
             return 'buy'
+    
+    def back_test(self, candles_before) -> list:
+        test_df = self.df.iloc[-candles_before:]
+        actions = []
+        for index, row in test_df.iterrows():
+            if row['BBHI']:
+                actions.append((datetime.fromtimestamp(row['time']).strftime('%Y-%m-%d %H:%M:%S'), row['BBHI'], 'sell'))
+            elif row['BBLI']:
+                actions.append((datetime.fromtimestamp(row['time']).strftime('%Y-%m-%d %H:%M:%S'), row['BBLI'], 'buy'))
+            else:
+                actions.append((datetime.fromtimestamp(row['time']).strftime('%Y-%m-%d %H:%M:%S'), 'hold'))
+        return actions
 
 
 class BBandRSI(Strategy):
-    pass
+    def __init__(self, df, bb_period, bb_dev_period, rsi_period) -> None:
+        self.df = df
+        self.rsi = RSI(df, period=rsi_period)
+        self.bb = BollingerBand(df, period=bb_period, dev_period=bb_dev_period)
+    
+    def apply_indicator(self) -> None:
+        self.rsi.apply_indicator()
+        self.bb.apply_indicator()
+    
+    def action(self) -> str:
+        if self.rsi.action(upperbound=75, lowerbound=25) == 'sell' and self.bb.action() == 'sell':
+            return 'sell'
+        elif self.rsi.action(upperbound=75, lowerbound=25) == 'buy' and self.bb.action() == 'buy':
+            return 'buy'
+        return 'hold'
+    
+    def back_test(self, candles_before) -> list:
+        actions = []
+        rsi_tests = self.rsi.back_test(candles_before, upperbound=75, lowerbound=25)
+        bb_tests = self.bb.back_test(candles_before)
+        for rsi_test in rsi_tests:
+            for bb_test in bb_tests:
+                if rsi_test[0] == bb_test[0]:
+                    if rsi_test[-1] == bb_test[-1] == 'sell':
+                        actions.append((rsi_test[0], 'sell'))
+                    elif rsi_test[-1] == bb_test[-1] == 'buy':
+                        actions.append((rsi_test[0], 'buy'))
+                    else:
+                        actions.append((rsi_test[0], 'hold'))
+        return actions
--- trader.py
+++ trader.py
@@ -1,22 +1,18 @@
 from datetime import datetime, timedelta
 from time import sleep
 from nobitex import Nobitex
-from signals import get_signal, back_test_signal
+from strategy import BBandRSI
 
 market = 'XLMIRT'
 nt = Nobitex()
 df = nt.get_ohlc(market, '60', datetime.now() - timedelta(days=30), datetime.now())
-while True:
-    print('new loop')
-    back_test_signal(df)
-    # if datetime.now().minute == 1:
-    #     df = nt.get_ohlc(market, '60', datetime.now() - timedelta(days=2), datetime.now())
-    # signal = get_signal(df)
-    # print(signal)
-    # if signal['buy']:
-    #     print('auto buying')
-    # elif signal['sell']:
-    #     print('auto selling')
-    # else:
-    #     print('not sure what to do, holding')
 
+print('new loop')
+bb_rsi_strat = BBandRSI(df, 30, 2, 13)
+bb_rsi_strat.apply_indicator()
+test = bb_rsi_strat.back_test(10*24)
+for item in test:
+    print(item)
+    # bb_rsi_strat = BBandRSI(df, 30, 2, 13)
+    # bb_rsi_strat.apply_indicator()
+    # print(datetime.now(), bb_rsi_strat.action())
